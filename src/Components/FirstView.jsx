76244e98fa653ddf4161e0d0753d3ac4f577945d
skshm2000 <107468465+skshm2000@users.noreply.github.com>
2022-12-07 14:57:27 +0530

    Update 0712

--- src/Components/FirstView.jsx
+++ src/Components/FirstView.jsx
@@ -11,11 +11,18 @@ export default function FirstView() {
         pt='14%'
         direction={{base:"column", sm:"column", md:"row", lg:"row"}} 
         spacing="100px" 
-        w={{base:"85%", sm:"85%", md:"95%", lg:"85%"}}
+        w={{base:"100%", sm:"85%", md:"95%", lg:"100%"}}
         >
             <Stack 
                 className="mainText" 
-                mt={{base:"170px", sm:"80px", md:"100px", lg:"110px"}} 
+                // m={{ 
+                //     base:'auto',
+                //     sm: 'auto',
+                //     md: '0',
+                //     lg: '0',
+                //     xl: '0',
+                //     '2xl': '0'}}
+                mt={{base:"270px", sm:"80px", md:"100px", lg:"110px"}} 
                 direction="column"
             >
                 <Heading textAlign={
@@ -26,7 +33,9 @@ export default function FirstView() {
                         lg: 'left',
                         xl: 'left',
                         '2xl': 'left'}
-                }>You have reached <span className='stylerFont'>{"<"}Saksham Selwal{"/>"}</span></Heading>
+                }>
+                    You have reached <span className='stylerFont'>{"<"} Saksham Selwal {"/>"}</span>
+                </Heading>
                 <Text textAlign={
                     { 
                         base:'center',
@@ -35,20 +44,40 @@ export default function FirstView() {
                         lg: 'left',
                         xl: 'left',
                         '2xl': 'left'}
-                }>Aspiring Full Stack Web Developer.</Text>
-                <Button onClick={()=>{
+                }>
+                    Aspiring Full Stack Web Developer.
+                </Text>
+                <Button 
+                onClick={()=>{
                     window.open('https://drive.google.com/file/d/1kKVSOq67udDMZX88g1ujntdrjIjTTzYg/view?usp=sharing')
                 }} 
                 colorScheme="blackAlpha"
-                w='50%'
-                >View Resume</Button>
+                w='45%'
+                alignSelf={{ 
+                    base:'center',
+                    sm: 'center',
+                    md: 'left',
+                    lg: 'left',
+                    xl: 'left',
+                    '2xl': 'left'}}
+                >
+                    View Resume
+                </Button>
             </Stack>
         </Stack>
         <Image 
             classname="bgImage"
             alignSelf="center"
             w="100%"
-            h="640px"
+            h={
+                { 
+                    base:'850px',
+                    sm: '850px',
+                    md: '640px',
+                    lg: '640px',
+                    xl: '640px',
+                    '2xl': '640px'}
+            }
             src="https://thumbs.gfycat.com/BitesizedDesertedBallpython-size_restricted.gif" 
         />
         </div>
